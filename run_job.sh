0bf617703e487eb6618987cb9e3114c8f277da6b
Hongchen Luo <luohongchen1993@gmail.com>
2025-08-08 15:55:51 +0200

    good

--- run_job.sh
+++ run_job.sh
@@ -16,7 +16,7 @@ fi
 
 # 在子进程中执行主逻辑
 (
-  echo "[wait_then_run.sh] Started watcher for PID $target_pid"
+  echo "[wait_then_run.sh] Started to wait for PID $target_pid"
 
   # 等待目标进程结束
   while kill -0 "$target_pid" 2>/dev/null; do
@@ -29,6 +29,7 @@ fi
 
 # 输出这个 watcher 脚本本身的 PID
 watcher_pid=$!
-echo "[wait_then_run.sh] Background watcher PID: $watcher_pid"
+echo "[wait_then_run.sh] wait PID: $target_pid | job PID: $watcher_pid | job command: $post_command"
 # also dump to log file
-echo "[wait_then_run.sh] Background watcher PID: $watcher_pid" >> /root/experiments/job_queue/jobs.log 2>&1
+echo "[wait_then_run.sh] wait PID: $target_pid | job PID: $watcher_pid | job command: $post_command" >> /root/experiments/job_queue/jobs.log 2>&1
+
